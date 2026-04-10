f227c4ff14b115aa5f577acf32932bd70c4c8a5c
sdorignac-a11y <s.dorignac@scms.edu.ar>
2026-04-10 19:47:44 -0300

    Update panel-auth.js

--- panel-auth.js
+++ panel-auth.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase.js?v=6";
+import { auth, db } from "from "./firebase.js?v=7";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
