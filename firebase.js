90a2aba3a1533355533000d35e72c8fcb4c304db
sdorignac-a11y <s.dorignac@scms.edu.ar>
2026-04-10 18:00:35 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,17 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBD2r0-IMOKOjc92I75AZx9czBsnUt7wd0",
+  apiKey: "TU_API_KEY",
   authDomain: "eduvia-97008.firebaseapp.com",
   projectId: "eduvia-97008",
   storageBucket: "eduvia-97008.firebasestorage.app",
   messagingSenderId: "1052924233848",
-  appId: "1:1052924233848:web:d19b4177fc64890042c58d"
+  appId: "EL_APP_ID_EXACTO_QUE_TE_DA_FIREBASE"
 };
 
 const app = initializeApp(firebaseConfig);
-
-console.log("Firebase iniciado:", firebaseConfig.projectId);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
