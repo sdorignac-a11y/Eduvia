97e0d374dbb020b1e1bfb145a764336edbb51688
sdorignac-a11y <s.dorignac@scms.edu.ar>
2026-04-10 18:20:25 -0300

    Update crear-clase.js

--- crear-clase.js
+++ crear-clase.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase.js";
+import { auth, db } from "./firebase.js?v=3"
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
