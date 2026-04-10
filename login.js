140fd02c2b3f12f3db1d439bba3412c2304ac3d9
sdorignac-a11y <s.dorignac@scms.edu.ar>
2026-04-10 13:16:58 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const form = document.getElementById("login-form");
 
