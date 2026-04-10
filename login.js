22f1032b7291eaa5e893e21281840650de988b20
sdorignac-a11y <s.dorignac@scms.edu.ar>
2026-04-10 12:37:56 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,24 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log("Usuario logueado:", userCredential.user);
+
+    // Redirigir al panel
+    window.location.href = "panel.html";
+
+  } catch (error) {
+    console.error(error);
+
+    alert("Error al iniciar sesión: " + error.message);
+  }
+});
